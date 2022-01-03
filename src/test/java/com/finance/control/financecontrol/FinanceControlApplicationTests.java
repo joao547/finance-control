fe124946c7042a47fbbf329268cc6856ad13559c
joao547 <joaobezerra547@gmail.com>
2022-01-03 11:08:50 -0300

    first commit

---
+++ src/test/java/com/finance/control/financecontrol/FinanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.control.financecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
